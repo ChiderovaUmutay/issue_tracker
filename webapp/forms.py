9f0cf57b122481f20f3f65867fe96d5bad012c89
Chiderova Umutay <ciderovaumutaj@gmail.com>
2022-07-07 14:54:59 +0600

    Написала форму для создания и редактирования

---
+++ webapp/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+from webapp.models import Task
+
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = "__all__"
+        error_messages = {
+            "summary": {
+                "required": "Поле обязательно для заполнения"
+            },
+            "status": {
+                "required": "Поле обязательно для заполнения"
+            },
+            "type": {
+                "required": "Поле обязательно для заполнения"
+            }
+        }
