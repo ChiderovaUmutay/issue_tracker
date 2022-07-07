300cad74a0a5eb538a8e24d5676a7e20796322e0
Chiderova Umutay <ciderovaumutaj@gmail.com>
2022-07-07 13:47:13 +0600

    Зарегестрировала модель 'Task' в понели администратора и создала суперадмина

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from webapp.models import Task
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'summary', 'status', 'created_at']
+    list_display_links = ['summary']
+    list_filter = ['status']
+    search_fields = ['summary', 'status', 'type']
+    fields = ['summary', 'description', 'status', 'type', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+admin.site.register(Task, TaskAdmin)
