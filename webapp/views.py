f2996803515a61f66aa61563a23e0e540218a135
Chiderova Umutay <ciderovaumutaj@gmail.com>
2022-07-07 14:32:38 +0600

    Написала представление 'TaskView' для детального просмотра и зарегестрировала его в 'webapp.urls'

--- webapp/views.py
+++ webapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.views.generic import TemplateView
@@ -12,4 +12,14 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         tasks = Task.objects.order_by("-status")
         kwargs["tasks"] = tasks
+        return super().get_context_data(**kwargs)
+
+
+class TaskView(TemplateView):
+    template_name = "task_view.html"
+
+    def get_context_data(self, **kwargs):
+        pk = kwargs.get("pk")
+        task = get_object_or_404(Task, pk=pk)
+        kwargs["task"] = task
         return super().get_context_data(**kwargs)
