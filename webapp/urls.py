c0a5eda72733c39d6262c17a4f15290ff8fed7c0
Chiderova Umutay <ciderovaumutaj@gmail.com>
2022-07-07 15:14:13 +0600

    Написала представление для редактирования и зарегестрировала его в 'webapp.urls'

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from webapp.views import IndexView, TaskView, CreateTask
+from webapp.views import IndexView, TaskView, CreateTask, UpdateTask
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('task/<int:pk>/view', TaskView.as_view(), name='task_view'),
     path('tasks/add/', CreateTask.as_view(), name='create_task'),
+    path('task/<int:pk>/update', UpdateTask.as_view(), name='update_task'),
 ]
