96ab2d2ac1e74551b8c8a3858356845dd75f2ba3
Chiderova Umutay <ciderovaumutaj@gmail.com>
2022-07-07 14:16:15 +0600

    Написала представление 'IndexView' для просмотра списка статей и зарегестрировала его в 'webapp.urls'

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import TemplateView
+
+from webapp.models import Task
+
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        tasks = Task.objects.order_by("-status")
+        kwargs["tasks"] = tasks
+        return super().get_context_data(**kwargs)
