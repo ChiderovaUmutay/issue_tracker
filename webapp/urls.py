004f8f7b3a25c1d7eaed171dace0cd5468e24128
Chiderova Umutay <ciderovaumutaj@gmail.com>
2022-07-07 14:55:45 +0600

    Написала представление 'CreateTask' для создания и зарегестрировала его в 'webapp.urls'

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from webapp.views import IndexView, TaskView
+from webapp.views import IndexView, TaskView, CreateTask
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('task/<int:pk>/view', TaskView.as_view(), name='task_view'),
+    path('tasks/add/', CreateTask.as_view(), name='create_task'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
+from django.views import View
 from django.views.generic import TemplateView
 
+from webapp.forms import TaskForm
 from webapp.models import Task
 
 
@@ -10,7 +12,7 @@ class IndexView(TemplateView):
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
-        tasks = Task.objects.order_by("-status")
+        tasks = Task.objects.order_by("-updated_at")
         kwargs["tasks"] = tasks
         return super().get_context_data(**kwargs)
 
@@ -23,3 +25,16 @@ class TaskView(TemplateView):
         task = get_object_or_404(Task, pk=pk)
         kwargs["task"] = task
         return super().get_context_data(**kwargs)
+
+
+class CreateTask(View):
+    def get(self, request):
+        form = TaskForm()
+        return render(request, "create.html", {"form": form})
+
+    def post(self, request):
+        form = TaskForm(data=request.POST)
+        if form.is_valid():
+            new_task = form.save()
+            return redirect("task_view", pk=new_task.pk)
+        return render(request, "create.html", {"form": form})
