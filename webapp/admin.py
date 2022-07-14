7a95d8d415c1f88d0fbcd956def3219472791ba2
Chiderova Umutay <ciderovaumutaj@gmail.com>
2022-07-14 18:31:15 +0600

    Добавила в admin.py функционал для удобного создания и отображения типов на странице задач

--- webapp/admin.py
+++ webapp/admin.py
@@ -4,15 +4,24 @@ from django.contrib import admin
 from webapp.models import Task, Type, Status
 
 
+class TypeInline(admin.TabularInline):
+    model = Task.type.through
+
+
 class TaskAdmin(admin.ModelAdmin):
     list_display = ['id', 'summary', 'status', 'created_at']
     list_display_links = ['summary']
     list_filter = ['status']
     search_fields = ['summary', 'status', 'type']
-    fields = ['summary', 'description', 'status', 'type', 'created_at', 'updated_at']
+    fields = ['summary', 'description', 'status', 'created_at', 'updated_at']
+    inlines = [TypeInline]
     readonly_fields = ['created_at', 'updated_at']
 
 
+class TypeAdmin(admin.ModelAdmin):
+    inlines = [TypeInline]
+
+
 admin.site.register(Task, TaskAdmin)
-admin.site.register(Type)
+admin.site.register(Type, TypeAdmin)
 admin.site.register(Status)
