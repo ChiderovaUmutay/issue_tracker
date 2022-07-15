1b6e2c4261c85aa535fc890b53cfc80315a42d7e
Chiderova Umutay <ciderovaumutaj@gmail.com>
2022-07-15 12:12:35 +0600

    Добавила валидации для полей summary и description

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,4 +1,7 @@
+import re
+
 from django import forms
+from django.core.exceptions import ValidationError
 from django.forms import widgets
 
 from webapp.models import Task
@@ -19,3 +22,15 @@ class TaskForm(forms.ModelForm):
         widgets = {
             "type": widgets.CheckboxSelectMultiple
         }
+
+    def clean_summary(self):
+        summary = self.cleaned_data.get("summary")
+        if not re.match("^[\w\s]+$", summary):
+            raise ValidationError("Заголовок должен состоять из букв или букв с цифрами")
+        return summary
+
+    def clean_description(self):
+        description = self.cleaned_data.get("description")
+        if len(description) <= 1:
+            raise ValidationError("Описание не должно быть короче одного символа")
+        return description
